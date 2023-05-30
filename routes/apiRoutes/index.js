2dad096d16ba367a56f863b1b6b24849ef347102
Frank-Colorado <chawnkiee@gmail.com>
2023-05-30 19:33:20 -0400

    adding routing in apiRoutes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,3 +1,15 @@
 const router = require("express").Router();
 
+// This route is for the /api/users endpoint
+const userRoutes = require("./userRoutes");
+router.use("/users", userRoutes);
+
+// This route is for the /api/posts endpoint
+const postRoutes = require("./postRoutes");
+router.use("/posts", postRoutes);
+
+// This route is for the /api/comments endpoint
+const commentRoutes = require("./commentRoutes");
+router.use("/comments", commentRoutes);
+
 module.exports = router;
