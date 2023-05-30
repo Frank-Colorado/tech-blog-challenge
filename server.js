a4d1f7453408362ab861f9d2634aeb77c17c7c2d
Frank-Colorado <chawnkiee@gmail.com>
2023-05-30 19:19:03 -0400

    installed luxon package

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const session = require("express-session");
 const routes = require("./routes");
-//const helpers = require("./utils/helpers");
-// const hbs = exphbs.create({
-//     helpers
-// });
+const helpers = require("./utils");
+const hbs = exphbs.create({
+  helpers,
+});
 const sequelize = require("./config/connection");
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-// app.engine("handlebars", hbs.engine);
+app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 const sessionConfig = {
