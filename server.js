8c5e9beef62e355987281815279f6b28b888bc74
Frank-Colorado <chawnkiee@gmail.com>
2023-05-30 19:12:45 -0400

    added base code to server.js file

---
+++ server.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+//const routes = require("./routes");
+//const helpers = require("./utils/helpers");
+// const hbs = exphbs.create({
+//     helpers
+// });
+const sequelize = require("./config/connection");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+const sessionConfig = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+
+// MIDDLEWARES
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+
+app.use(session(sessionConfig));
+
+// ROUTES
+//app.use("/", routes);
+
+// SERVER LISTENER
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+});
