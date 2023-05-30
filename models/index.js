ff3d50c526ca2b2625db8538a46111c72228254b
Frank-Colorado <chawnkiee@gmail.com>
2023-05-30 19:57:07 -0400

    created Associations for all Models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,39 @@
 const User = require("./User");
 const Post = require("./Post");
 const Comment = require("./Comment");
+
+// User Associations with Post
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  foreignKey: "author",
+  onDelete: "CASCADE",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+  foreignKey: "author",
+});
+
+// User Associations with Comment
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  foreignKey: "author",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  foreignKey: "author",
+});
+
+// Post Associations with Comment
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Comment };
