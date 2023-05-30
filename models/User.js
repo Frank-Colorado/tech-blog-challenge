83223c10ed8eb63faf1c11885c7e2d3cab95bd51
Frank-Colorado <chawnkiee@gmail.com>
2023-05-30 19:38:41 -0400

    created User class w/ instance method

--- models/User.js
+++ models/User.js
@@ -1,3 +1,15 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 const bcrypt = require("bcrypt");
+
+class User extends Model {
+  // This is an instance method called 'checkPassword'
+  // It has 1 parameter: 'userPw'
+  async checkPassword(userPw) {
+    try {
+      return await bcrypt.compare(userPw, this.password);
+    } catch (err) {
+      console.log({ err });
+    }
+  }
+}
