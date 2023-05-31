defb50fa02f22b29c662d7b9c0609f57655afce4
Frank-Colorado <chawnkiee@gmail.com>
2023-05-30 20:12:27 -0400

    added get route for '/' in htmlRoutes.js

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,3 +1,7 @@
 const router = require("express").Router();
+const { displayHome } = require("../../controllers/htmlControllers");
+
+// This route will display the home page
+router.get("/", displayHome);
 
 module.exports = router;
