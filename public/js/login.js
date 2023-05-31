9d0f441f19c3ff797bd44123dd98b57fe7c7e5b0
Frank-Colorado <chawnkiee@gmail.com>
2023-05-30 22:09:13 -0400

    created 'alertDisplay' function in login.js file

--- public/js/login.js
+++ public/js/login.js
@@ -3,3 +3,10 @@ const $username = document.getElementById("username");
 const $password = document.getElementById("password");
 const $loginBtn = document.getElementById("loginBtn");
 const $alert = document.getElementById("alert");
+
+// This is a function called alertDisplay
+// It has 1 parameter: 'message'
+// It will display the message in the alert box
+const alertDisplay = (message) => {
+  $alert.textContent = message;
+};
