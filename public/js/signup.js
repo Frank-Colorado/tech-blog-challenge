415ef8ceef8590b51e0baa76bdde0a852dd24bb1
Frank-Colorado <chawnkiee@gmail.com>
2023-05-30 22:46:33 -0400

    created alertDisplay function in signup.js file

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,3 +3,10 @@ const $username = document.getElementById("username");
 const $password = document.getElementById("password");
 const $signupBtn = document.getElementById("signupBtn");
 const $alert = document.getElementById("alert");
+
+// This is a function called alertDisplay
+// It has 1 parameter: 'message'
+// It will display the message in the alert box
+const alertDisplay = (message) => {
+  $alert.textContent = message;
+};
