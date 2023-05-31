2b1014a290e729588c17723a349d57262af11428
Frank-Colorado <chawnkiee@gmail.com>
2023-05-30 22:27:27 -0400

    created 'signInUser' async function in login.js

--- public/js/login.js
+++ public/js/login.js
@@ -11,6 +11,39 @@ const alertDisplay = (message) => {
   $alert.textContent = message;
 };
 
+// This is a function called sendLogin
+// It has 1 parameter: 'user'
+// It will send the user object to the server
+const signInUser = async (user) => {
+  try {
+    // We destructure the username and password from the user object
+    const { username, password } = user;
+    // We send a POST request to the server
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, password }),
+    });
+    // We wait for the response from the server
+    // We convert the response to JSON and store it in a variable called 'data'
+    const data = await response.json();
+    // If the response is ok, we redirect the user to the dashboard page
+    if (response.ok) {
+      const { user, message } = data;
+      console.log({ user, message });
+      location.href = "/dashboard";
+    } else {
+      const { error } = data;
+      // If the login fails, we call the alertDisplay function
+      // We pass it the error message
+      alertDisplay(error);
+    }
+  } catch (err) {
+    // This will handle any errors on the server side
+    console.error({ err });
+  }
+};
+
 // This is an event listener for the login button
 $loginBtn.addEventListener("click", (e) => {
   // we prevent the default behaviour of the form
@@ -34,7 +67,7 @@ $loginBtn.addEventListener("click", (e) => {
     username,
     password,
   };
-  // We call the sendLogin function
+  // We call the signInUser function
   // We pass it the user object
-  sendLogin(user);
+  signInUser(user);
 });
