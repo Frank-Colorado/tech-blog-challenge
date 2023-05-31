555553e17ee15b0b976d599155835a8c69348daa
Frank-Colorado <chawnkiee@gmail.com>
2023-05-30 22:53:46 -0400

    added event function for signup btn in signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -10,3 +10,29 @@ const $alert = document.getElementById("alert");
 const alertDisplay = (message) => {
   $alert.textContent = message;
 };
+
+// This is an event listener for the signup button
+$signupBtn.addEventListener("click", (e) => {
+  // we prevent the default behaviour of the form
+  e.preventDefault();
+  // We set the alert box to be empty
+  $alert.textContent = "";
+  // We get the values of the username and password fields
+  const username = $username.value;
+  const password = $password.value.trim();
+  // We check if the username and password are empty
+  if (!username || !password) {
+    // If they are, we call the alertDisplay function
+    // We pass it the message "Please enter a username and password"
+    alertDisplay("Please enter a username and password");
+  }
+
+  // We create a user object
+  const user = {
+    username,
+    password,
+  };
+  // We call the signUpUser function
+  // We pass it the user object
+  signUpUser(user);
+});
