cbb320ebe888ee9037e524d733c6140562a10434
Frank-Colorado <chawnkiee@gmail.com>
2023-05-30 22:45:50 -0400

    declared variables in signup.js file

---
+++ public/js/signup.js
@@ -0,0 +1,5 @@
+const $signupForm = document.getElementById("signupForm");
+const $username = document.getElementById("username");
+const $password = document.getElementById("password");
+const $signupBtn = document.getElementById("signupBtn");
+const $alert = document.getElementById("alert");
