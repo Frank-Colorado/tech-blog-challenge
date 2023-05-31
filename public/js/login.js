2f0279bf2669911bbe112ad52bf3b63f5a27e921
Frank-Colorado <chawnkiee@gmail.com>
2023-05-30 22:07:39 -0400

    declared variables in login.js file

---
+++ public/js/login.js
@@ -0,0 +1,5 @@
+const $loginForm = document.getElementById("loginForm");
+const $username = document.getElementById("username");
+const $password = document.getElementById("password");
+const $loginBtn = document.getElementById("loginBtn");
+const $alert = document.getElementById("alert");
