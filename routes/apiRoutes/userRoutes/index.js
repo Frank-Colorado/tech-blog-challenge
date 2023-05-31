6f47b1f4c92186e057ac195eb622d7606cde5cc2
Frank-Colorado <chawnkiee@gmail.com>
2023-05-30 21:46:24 -0400

    created routes in userRoutes

--- routes/apiRoutes/userRoutes/index.js
+++ routes/apiRoutes/userRoutes/index.js
@@ -1,3 +1,17 @@
 const router = require("express").Router();
+const {
+  createUser,
+  loginUser,
+  logoutUser,
+} = require("../../../controllers/apiControllers/userController");
+
+// This route will be called with /api/users/signup
+router.post("/signup", createUser);
+
+// This route will be called with /api/users/login
+router.post("/login", loginUser);
+
+// This route will be called with /api/users/logout
+router.post("/logout", logoutUser);
 
 module.exports = router;
