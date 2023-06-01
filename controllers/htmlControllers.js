b2b3430d96fd8a2f6e4fbff1c650e6ba408ec16b
Frank-Colorado <chawnkiee@gmail.com>
2023-06-01 00:26:14 -0400

    added code to 'displayHome' in htmlControllers

--- controllers/htmlControllers.js
+++ controllers/htmlControllers.js
@@ -1,14 +1,25 @@
 const { User, Post, Comment } = require("../models");
 
 // This is a function that will display the home page
-const displayHome = (req, res) => {
-  res.render("home", {
-    nav: true,
-    logged_in: req.session.logged_in,
-    header: "Tech Blog",
-  });
+const displayHome = async (req, res) => {
+  try {
+    // Grab all the posts from the Post model
+    const postData = await Post.findAll();
+    // Serialize the data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+    // render the home template and pass the posts in as an object
+    res.render("home", {
+      posts,
+      nav: true,
+      logged_in: req.session.logged_in,
+      header: "Tech Blog",
+    });
+  } catch (err) {
+    console.log("Problem with displayHome function");
+    console.log({ err });
+    res.status(500).json(err);
+  }
 };
-
 // This is a function that will display the login page
 const displayLogin = (req, res) => {
   if (req.session.logged_in) {
