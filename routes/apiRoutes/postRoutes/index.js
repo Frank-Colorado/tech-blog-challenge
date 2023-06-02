2e679ed4c5de867608d3139df790c66700521dc7
Frank-Colorado <chawnkiee@gmail.com>
2023-06-01 23:22:48 -0400

    added routing for CRUD functions in postRoutes

--- routes/apiRoutes/postRoutes/index.js
+++ routes/apiRoutes/postRoutes/index.js
@@ -1,3 +1,14 @@
 const router = require("express").Router();
+const {
+  createPost,
+  updatePost,
+  deletePost,
+} = require("../../../controllers/apiControllers/postController");
+
+// This route will be called with /api/posts
+router.post("/", createPost);
+
+// These route will be called with /api/posts/:id
+router.route("/:id").put(updatePost).delete(deletePost);
 
 module.exports = router;
