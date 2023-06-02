540b226faebe3c15024696d2f7200d64f1b66c31
Frank-Colorado <chawnkiee@gmail.com>
2023-06-02 19:23:02 -0400

    created updatePostBtn click event function

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -3,3 +3,21 @@ const $postTitle = document.getElementById("title");
 const $postContent = document.getElementById("content");
 const $updatePostBtn = document.getElementById("updateBtn");
 const $deletePostBtn = document.getElementById("deleteBtn");
+
+// This is a click listener for the update button
+$updatePostBtn.addEventListener("click", () => {
+  // We set any previous alerts to empty
+  $alert.textContent = "";
+  // We get the values from the input fields
+  const title = $postTitle.value;
+  const content = $postContent.value;
+  const id = $postDiv.getAttribute("data-id");
+  // We create a new post object
+  const post = {
+    id,
+    title,
+    content,
+  };
+  // We call the sendPost function and pass in the post object
+  updatePost(post);
+});
