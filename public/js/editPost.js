936cb5b8be18a8d2fba8a4a9bffd819cd1ed1bb6
Frank-Colorado <chawnkiee@gmail.com>
2023-06-02 19:28:21 -0400

    created deletePost function in editPost.js

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -4,6 +4,9 @@ const $postContent = document.getElementById("content");
 const $updatePostBtn = document.getElementById("updateBtn");
 const $deletePostBtn = document.getElementById("deleteBtn");
 
+// This is a function called updatePost
+// It has 1 parameter: 'post'
+// It will send the post object to the server to be updated
 const updatePost = async (post) => {
   try {
     // We destructure the post parameter
@@ -15,6 +18,32 @@ const updatePost = async (post) => {
       headers: { "Content-Type": "application/json" },
       // The body of the request is the post object
       body: JSON.stringify({ title, content }),
+    });
+    // We wait for the response from the server
+    const data = await response.json();
+    // If the response is ok
+    if (response.ok) {
+      // We redirect to the dashboard page
+      location.href = "/dashboard";
+    } else {
+      const { error } = data;
+      alertDisplay(error);
+    }
+  } catch (err) {
+    console.error({ err });
+  }
+};
+
+// This is a function called deletePost
+// It has 1 parameter: 'id'
+// It will send the id of the Post to be deleted to the server
+const deletePost = async (id) => {
+  try {
+    // We send a DELETE request to the server
+    // We pass in the ID as a parameter in the URL
+    const response = await fetch(`/api/posts/${id}`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
     });
     // We wait for the response from the server
     const data = await response.json();
@@ -47,4 +76,14 @@ $updatePostBtn.addEventListener("click", () => {
   };
   // We call the sendPost function and pass in the post object
   updatePost(post);
+});
+
+$deletePostBtn.addEventListener("click", () => {
+  // We set any previous alerts to empty
+  $alert.textContent = "";
+  // We get the id of the post
+  const id = $postDiv.getAttribute("data-id");
+
+  // We call the deletePost function and pass in the id
+  deletePost(id);
 });
