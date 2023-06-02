3907a86529d7c8790cc77002fab46e22c9bdd3ae
Frank-Colorado <chawnkiee@gmail.com>
2023-06-02 19:20:24 -0400

    declared variables in editPost.js

---
+++ public/js/editPost.js
@@ -0,0 +1,5 @@
+const $postDiv = document.getElementById("postDiv");
+const $postTitle = document.getElementById("title");
+const $postContent = document.getElementById("content");
+const $updatePostBtn = document.getElementById("updateBtn");
+const $deletePostBtn = document.getElementById("deleteBtn");
