69c25c9e248b89d55c403ad0729c0c4832660a00
Frank-Colorado <chawnkiee@gmail.com>
2023-06-01 23:04:11 -0400

    created 'sendComment' function in post.js

--- public/js/post.js
+++ public/js/post.js
@@ -1,6 +1,37 @@
 const $commentBtn = document.getElementById("commentBtn");
 const $commentInput = document.getElementById("commentInput");
 
+// This is an async function called sendComment
+// It has 1 parameter: 'comment'
+// It will send the comment object to the server
+const sendComment = async (comment) => {
+  try {
+    // We destructure the content and post_id from the comment object
+    const { content, post_id } = comment;
+    // We send a POST request to the server
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      // The body of the request is the comment object
+      body: JSON.stringify({ content, post_id }),
+    });
+    // We wait for the response from the server
+    const data = await response.json();
+    // If the response is ok
+    if (response.ok) {
+      // We set the comment input field to be empty
+      $commentInput.value = "";
+      // We reload the page
+      location.reload();
+    } else {
+      const { error } = data;
+      alertDisplay(error);
+    }
+  } catch (err) {
+    console.error({ err });
+  }
+};
+
 $commentBtn.addEventListener("click", (e) => {
   // we prevent the default behaviour of the form
   e.preventDefault();
