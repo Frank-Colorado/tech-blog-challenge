c682b14b82223ae541e4d6e5ad23da33fa9ad161
Frank-Colorado <chawnkiee@gmail.com>
2023-06-01 23:12:33 -0400

    created routing for comment POST req

--- routes/apiRoutes/commentRoutes/index.js
+++ routes/apiRoutes/commentRoutes/index.js
@@ -1,3 +1,9 @@
 const router = require("express").Router();
+const {
+  createComment,
+} = require("../../../controllers/apiControllers/commentController");
+
+// This route will be called with /api/comments
+router.post("/", createComment);
 
 module.exports = router;
